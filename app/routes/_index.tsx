8a4371b4d5c1c493801c220b6620e048f7ed82b2
Mikey Nichols <mnix@journeytocode.io>
2024-06-23 22:26:18 -0400

    adds an index route and page

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        Welcome to your new Remix app!
+        <br />
+        Check out{" "}
+        <a href="https://remix.run">the docs at remix.run</a>.
+      </p>
+    );
+  }
+  
